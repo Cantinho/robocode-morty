b08160eec6cbe999c4fe9018c89358076cf1a63a
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2019-12-05 00:04:18 -0300

    add simple aiming

--- cantinho/controllers/gun/GunController.java
+++ cantinho/controllers/gun/GunController.java
@@ -1,23 +1,21 @@
 package cantinho.controllers.gun;
 
+import cantinho.controllers.IAimStrategy;
 import cantinho.controllers.IGun;
 import robocode.HitByBulletEvent;
 import robocode.ScannedRobotEvent;
 
-import static robocode.util.Utils.normalAbsoluteAngle;
-import static robocode.util.Utils.normalRelativeAngle;
-
 public class GunController {
 
     final IGun gun;
-
+    final IAimStrategy aimStrategy;
     private boolean isTracking = false;
 
-    // The coordinate of the radar target (x,y)
-    int gunTargetX, gunTargetY = Integer.MIN_VALUE;
-
-    public GunController(final IGun gun) {
+    public GunController(final IGun gun, final IAimStrategy aimStrategy) {
         this.gun = gun;
+        this.aimStrategy = aimStrategy;
+        assert(gun != null);
+        assert(aimStrategy != null);
     }
 
     public void init() {
@@ -25,11 +23,7 @@ public class GunController {
     }
 
     public void run() {
-        // Turns the gun toward the current aim coordinate (x,y) controlled by
-        // the current mouse coordinate
-        double angle = normalAbsoluteAngle(Math.atan2(gunTargetX - gun.getX(), gunTargetY - gun.getY()));
 
-        gun.setTurnGunRightRadians(normalRelativeAngle(angle - gun.getGunHeadingRadians()));
     }
 
     public void onHitByBullet(final HitByBulletEvent event) {
@@ -42,12 +36,7 @@ public class GunController {
         if(!isTracking) {
             isTracking = true;
         }
-        // Calculate the angle to the scanned robot
-        double angle = Math.toRadians((gun.getHeading() + event.getBearing()) % 360);
-
-        // Calculate the coordinates of the robot
-        gunTargetX = (int)(gun.getX() + Math.sin(angle) * event.getDistance());
-        gunTargetY = (int)(gun.getY() + Math.cos(angle) * event.getDistance());
+        aimStrategy.aim(event);
     }
 
 }
